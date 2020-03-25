e8671a9c7d071fa096b7adab1cbcf1e2b1b1b09f
swesi <swesi@gmx.de>
2020-03-25 15:02:45 +0100

    1 file modified

--- CarService.ts
+++ CarService.ts
@@ -5,12 +5,12 @@ export class CarService {
   constructor() {}
 
   async GetCars(): Promise<Car[]> {
-    var promise = new Promise<Car[]>((resolve, reject) => {
+    var promise = new Promise<Car[]>((res, reject) => {
       console.log("CarService: Promis created wait 3s");
       setTimeout(() => {
         console.log("Async is done!");
         var cars: Car[] = [new Car("BMW"), new Car("VW"), new Car("Mini")];
-        resolve(cars);        
+        res(cars);        
       }, 3000);
     });
     return promise
